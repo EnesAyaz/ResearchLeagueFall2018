2abe380c59faa738cd999bd52860fe04eb1442e7
Enes <enes.ayaz.1996@gmail.com>
2018-11-22 09:30:10 +0300

    small changes are made at GaN current bias

--- GaN Model/GaN/CurrentVias.m
+++ GaN Model/GaN/CurrentVias.m
@@ -1,9 +1,8 @@
-
 clc;
 clear all;
 close all;
 %% Simulation Parameters
-SampleTime = 1e-13; %Time Steps
+SampleTime = 1e-12; %Time Steps
 StopTime = 1000e-9; %Stop Time
 fsw = 3e6;
 t = (0 : SampleTime : StopTime);
@@ -16,37 +15,37 @@ Rd = (3.6/8) * (0.95*0.82*(1 - (-0.0135*(125 - 25))) * 18.2 / 295);
 [u1T, u1B] = PulseTimer(t,fsw,SampleTime);
 %%
 a=length(t);
-x1B = linspace(0,5,10000) ;%Ids
-x1B1=5*ones(1,a-10000);
-x1B=[x1B x1B1];
+u2CB = linspace(0,5,10000) ;%Ids
+u2B1=5*ones(1,a-10000);
+u2CB=[u2CB u2B1]; % Ids
 x3B = zeros(size(t)); %Vdsin
 x4B = zeros(size(t)); %Vgsin
 x7B = zeros(size(t)); %Ig
-u2B = zeros(size(t)); %Vdso
-% u1B= zeros(size(t)); %Vgso
-% u1B= 3*ones(size(t)); %Vgso
+%%u1B= zeros(size(t)); %Vgso
+Vdsout = zeros(size(t));
+u1B= 3*ones(size(t)); %Vgso
 x8=zeros(size(t)); %%Ich
 
 %%
 [~,n] = size(t);
 for k=3:n
    
-    [x8(k),x7B(k),x4B(k),x3B(k)] = StateSpaceGanCurrentInput(u1B(k),x1B(k),x7B(k-1),x4B(k-1),x3B(k-1),SampleTime);
-     u2B(k)= ((x1B(k)-x1B(k-1))/(SampleTime/(Ls+Ld))) + x3B(k)+ (x1B(k)*(Rs+Rd));
+    [x8(k),x7B(k),x4B(k),x3B(k)] = StateSpaceGanCurrentInput(u1B(k),u2CB(k),x7B(k-1),x4B(k-1),x3B(k-1),SampleTime);
+     Vdsout(k)= ((u2CB(k)-u2CB(k-1))/(SampleTime/(Ls+Ld))) + x3B(k)+ (u2CB(k)*(Rs+Rd));
      
 end
 %%
 figure(1);
 hold all
 grid on
-plot(t,x1B,'Linewidth',2.0);
+plot(t,u2CB,'Linewidth',2.0);
 legend ('Ids');
 hold off
 
 figure(2);
 hold all
 grid on
-plot(t,u2B,'Linewidth',2.0);
+plot(t,Vdsout,'Linewidth',2.0);
 legend ('Vbias');
 hold off
 
