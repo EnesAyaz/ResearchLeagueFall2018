6899de29d3f27a0ae4069c71d027d10c35a84a1a
Enes <enes.ayaz.1996@gmail.com>
2018-12-06 20:30:05 +0300

    updated to changeable scenerio

--- GaN Model/GaN/StateSpaceHalfBridge.m
+++ GaN Model/GaN/StateSpaceHalfBridge.m
@@ -1,16 +1,17 @@
 % State Variables
 % Id, Ig, Vgsin, Vdsin
 %% State Space Model of GaN
-% clear all;
-% close all;
+clear all;
+close all;
 
 %% Simulation Parameters
-SampleTime = 1e-13; %Time Steps
-StopTime = 100e-9; %Stop Time
+SampleTime = 1e-15; %Time Steps
+StopTime = 400e-9; %Stop Time
 
 %% Load Parameters
-Rload = 10e4;
-Lload = 40e-6;
+Rload = 100;
+Lload = 4-6;
+Cload=1e-12;
 fsw = 3e6;
 Lp = 1e-10;
 LdcP = 5e-9;
@@ -31,7 +32,10 @@ Rd = (3.6/8) * (0.95*0.82*(1 - (-0.0135*(125 - 25))) * 18.2 / 295);
 %% Input Definition
 t = (0 : SampleTime : StopTime);
 [u1T, u1B] = PulseTimer(t,fsw,SampleTime);
-% plot(t,u1T,t,u1B);
+figure;
+plot(t,u1T,t,u1B);
+legend('U1','U2');
+ylim([-6 10])
 %% Allocation
 t = (0 : SampleTime : StopTime);
 
@@ -42,7 +46,7 @@ x7T = zeros(size(t)); %Ig
 % u1T = zeros(size(t)); %Vgsso
 u2T = zeros(size(t)); %Vdso
 
-u = 100*ones(size(t)); %Vdc
+u = linspace(0,100,length(t));%Vdc
 Vload = zeros(size(t)); %Load Voltage
 Iload = zeros(size(t)); %Load Current
 
@@ -54,37 +58,56 @@ x7B = zeros(size(t)); %Ig
 % u1B = zeros(size(t)); %Vgsso
 u2B = zeros(size(t)); %Vdso
 x8=zeros(size(t)); %%Ich
-u1B= zeros(size(t));
+%u1B= zeros(size(t));
 
 %% Calculation
 [~,n] = size(t);
 for k=3:(n-3)
-    u2T(k)= u(k)-u2B(k-1);
-    [x1T(k),x7T(k),x4T(k),x3T(k)] = StateSpaceGaNBlock(u1T(k),u2T(k),x1T(k-1),x7T(k-1),x4T(k-1),x3T(k-1),SampleTime);
-    u2T(k)= (x1T(k)*(Rd+Rs) )+ (((x1T(k)-x1T(k-1))/SampleTime)*(Ls+Ld))+ x3T(k);
+%     u2T(k)= u(k)-u2B(k-1);
+     u2T(k)=u(k)-u2B(k-1);
+%      u2T(k)=u(k);
+
+     [x1T(k),x7T(k),x4T(k),x3T(k)] = StateSpaceGaNBlock(u1T(k),u2T(k),x1T(k-1),x7T(k-1),x4T(k-1),x3T(k-1),SampleTime);
+     
+         u2T(k)= (x1T(k)*(Rd+Rs) )+ (((x1T(k)-x1T(k-1))/SampleTime)*(Ls+Ld))+ x3T(k);
     Vload(k)=u(k)-u2T(k);
-    Iload(k)= (SampleTime*Vload(k)+ Lload*Iload(k-1))/(SampleTime*Rload+Lload);
+    Iload(k)= (SampleTime*Vload(k)/Lload+ Iload(k-1))/(((SampleTime*Rload)/Lload)+1);
     x1B(k)=x1T(k)-Iload(k);
     [x8(k),x7B(k),x4B(k),x3B(k)] = StateSpaceGanCurrentInput(u1B(k),x1B(k),x7B(k-1),x4B(k-1),x3B(k-1),SampleTime);
     u2B(k)= x3B(k)+ (x1B(k)- x1B(k-1))*(Ls+Ld)/SampleTime + x1B(k)* (Rs+Rd);
-   
-   
+
+     
+%      Vload(k)=Vload(k-1)+ (x1T(k)*SampleTime)/Cload;
+    
+    
+    % Vload(k)=((Lload*(x1T(k)-x1T(k-1))/SampleTime) + ((Lload/Rload)*Vload(k-1)))/ (1+Lload/Rload);
+    
 end
+    %% voltage
+%     Iload(k)=x1T(k)-x1B(k-1);
+%     Vload(k)= Iload(k)*Rload+ ((Iload(k)-Iload(k-1))*Lload)/SampleTime;
+%     u2B(k)=Vload(k);
+%     [x1B(k),x7B(k),x4B(k),x3B(k)] = StateSpaceGaNBlock(u1B(k),u2B(k),x1B(k-1),x7B(k-1),x4B(k-1),x3B(k-1),SampleTime);
+    
+    %% current
+   
+   
+
 
 %% Plot and See
 
-
-figure;
-hold all
-grid on
-plot(t,x1T,'Linewidth',2.0);
-%xlim([0]);
-%ylim([-50 120]);
-xlabel('Time');
-ylabel('Voltage,Ampere');
-title({'Top Switch Ids, Vds '})
-legend ('IdsT','VdsT','Location','best');
-hold off
+% 
+% figure;
+% hold all
+% grid on
+% plot(t,x1T,'Linewidth',2.0);
+% %xlim([0]);
+% %ylim([-50 120]);
+% xlabel('Time');
+% ylabel('Voltage,Ampere');
+% title({'Top Switch Ids, Vds '})
+% legend ('IdsT','VdsT','Location','best');
+% hold off
 
 % figure;
 % hold all
@@ -99,18 +122,13 @@ hold off
 % hold off
 
 
-% figure;
-% hold all
-% grid on
-% plot(t,x1B,t,u2B,t,x7B,t,u1B,'Linewidth',2.0);
-% %xlim([0]);
-% ylim([-50 120]);
-% xlabel('Time');
-% ylabel('Voltage,Ampere');
-% title({'Bottom Switch Ids, Vds, Ig, Vgs'})
-% legend ('Ids','Vds','Ig','Vgs','Location','best');
-% hold off
-% 
+figure;
+hold on;
+plot(t,x1T,t,u2T,t,Vload','Linewidth',2.0);
+legend ('IdsT','VdsT','Vload');
+
+hold off
+
 % figure;
 % hold all
 % grid on
